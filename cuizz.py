53e4151e6237ea01117a2a5f35449874a6059e17
strawluffy <cui_zz@126.com>
2018-04-06 20:09:15 +0800

    提交判断平台版本的信息

--- cuizz.py
+++ cuizz.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python
 import os
-print 'okzz'
+import sys
+platform_name = sys.platform
+if platform_name == 'win32':
+    print '平台是windows'
+elif platform_name == 'linux2':
+    print '平台是linux'
+else:
+    print 'unkown'
